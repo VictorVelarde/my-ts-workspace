1e49e05a1fb2466632c90572067f154bf60bb793
Víctor Velarde <victor.velarde@gmail.com>
2021-04-02 12:40:17 +0200

    Initial setup with cra + basic cleaning

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import { store } from '../app/store';
+import App from '../App';
 
-test('renders learn react link', () => {
+test('renders Home content', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/Hello world/i)).toBeInTheDocument();
 });
